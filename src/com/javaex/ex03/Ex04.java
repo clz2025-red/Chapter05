d9ad3c96e6f569ded835cf08120c318a387364bb
clz2025-red <clz2025.red@gmail.com>
2025-05-16 10:01:34 +0900

    2025-05-16

--- src/com/javaex/ex03/Ex04.java
+++ src/com/javaex/ex03/Ex04.java
@@ -1,28 +1,56 @@
 package com.javaex.ex03;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Ex04 {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		
-		/*
+		//리스트 만들기
+		List<Person> pList = new ArrayList<Person>();
+		
+		//스트림 준비
+		Reader fr = new FileReader("C:\\javaStudy\\PhoneDB.txt");
+		BufferedReader br = new BufferedReader(fr);
+		
+		
+		
+		
+		
+		//반복(파일이 끝까지)
+		while(true) {
+			//파일에서 1줄(이효리)을 읽는다
+			String str =br.readLine(); 
+			
+			//읽어줄에서 이름 hp company로 분리한다  
+			//"이효리,010-2222-3333,031-2323-4441" -->[이효리][010-2222-3333][031-2323-4441]
+			str.split(",");
+			
+			if(str == null) {
+				break;
+			}
+			
+			System.out.println(str);
+		}
+		
+		
+		
+	
+		
+		//Person을 메모리에 올리고 이름 hp company를 초기화한다
+		
+		//리스트에 Person주소를 넣는다
 		//반복
-		PhoneDB.txt 파일을 한줄씩 읽는다, (MS949방식으로 저장되어 있음)
-		
-		"이효리,010-2222-3333,031-2323-4441"  을 "," 로 구분한다.  -->배열
-		
-		
-		//반복
-		====>리스트에 정리
-		[0x111] [0x3333] [0x888]
-		
-		0x111  name = 이효리,  hp= 010-2222-3333, company=031-2323-4441  ==>Person
 		
 		
 		
-		for으로 출력
-		문제에 있는 형식으로 출력한다
 		
-		*/
+		
 		
 		
 
